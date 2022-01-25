060b4acbcb3bba4ec589fa4a3f567386b17017dc
skp96 <patel.sunny293@gmail.com>
2022-01-25 12:32:38 -0500

    enabled cors

--- lib/wine_o_inventory_api_web/endpoint.ex
+++ lib/wine_o_inventory_api_web/endpoint.ex
@@ -44,5 +44,6 @@ defmodule WineOInventoryApiWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug, origin: "*"
   plug WineOInventoryApiWeb.Router
 end
