493d1330b0d3da47904737bbd6d41feaed996724
skp96 <patel.sunny293@gmail.com>
2022-01-26 15:06:51 -0500

    removed code generated from phx.gen.context, expect list_wines function and tests related to it

--- lib/wine_o_inventory_api/products.ex
+++ lib/wine_o_inventory_api/products.ex
@@ -2,8 +2,6 @@ defmodule WineOInventoryApi.Products do
   @moduledoc """
   The Products context.
   """
-
-  import Ecto.Query, warn: false
   alias WineOInventoryApi.Repo
 
   alias WineOInventoryApi.Products.Wine
@@ -19,86 +17,5 @@ defmodule WineOInventoryApi.Products do
   """
   def list_wines do
     Repo.all(Wine)
-  end
-
-  @doc """
-  Gets a single wine.
-
-  Raises `Ecto.NoResultsError` if the Wine does not exist.
-
-  ## Examples
-
-      iex> get_wine!(123)
-      %Wine{}
-
-      iex> get_wine!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_wine!(id), do: Repo.get!(Wine, id)
-
-  @doc """
-  Creates a wine.
-
-  ## Examples
-
-      iex> create_wine(%{field: value})
-      {:ok, %Wine{}}
-
-      iex> create_wine(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_wine(attrs \\ %{}) do
-    %Wine{}
-    |> Wine.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a wine.
-
-  ## Examples
-
-      iex> update_wine(wine, %{field: new_value})
-      {:ok, %Wine{}}
-
-      iex> update_wine(wine, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_wine(%Wine{} = wine, attrs) do
-    wine
-    |> Wine.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a wine.
-
-  ## Examples
-
-      iex> delete_wine(wine)
-      {:ok, %Wine{}}
-
-      iex> delete_wine(wine)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_wine(%Wine{} = wine) do
-    Repo.delete(wine)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking wine changes.
-
-  ## Examples
-
-      iex> change_wine(wine)
-      %Ecto.Changeset{data: %Wine{}}
-
-  """
-  def change_wine(%Wine{} = wine, attrs \\ %{}) do
-    Wine.changeset(wine, attrs)
   end
 end
--- test/wine_o_inventory_api_web/views/api/wine_view_test.exs
+++ test/wine_o_inventory_api_web/views/api/wine_view_test.exs
@@ -1,12 +1,11 @@
 defmodule WineOInventoryApiWeb.WineViewTest do
     use WineOInventoryApiWeb.ConnCase, async: true
     alias WineOInventoryApiWeb.WineView
-    import WineOInventoryApi.ProductsFixtures
 
     describe "index.json render" do
         test "renders list of all wines" do
-            wine = wine_fixture()
-            wines = [wine]
+            fakeWine = %{id: 1, name: "some fake wine", description: "some fake description", rating: 5, quantity: 100 }
+            wines = [fakeWine]
             result_of_render = WineView.render("index.json", wines: wines)
             assert result_of_render != []
         end
