8a580ee244d7afc5bac4caa503d35a6837c2f88b
skp96 <patel.sunny293@gmail.com>
2022-02-08 09:37:18 -0500

    added the on_delete option to the Inventories migration per documentation

--- priv/repo/migrations/20220207214654_create_inventories.exs
+++ priv/repo/migrations/20220207214654_create_inventories.exs
@@ -3,7 +3,7 @@ defmodule WineOInventoryApi.Repo.Migrations.CreateInventories do
 
   def change do
     create table(:inventories) do
-      add :member_id, references(:members), null: false
+      add :member_id, references(:members, on_delete: :delete_all), null: false
       timestamps()
     end
   end
