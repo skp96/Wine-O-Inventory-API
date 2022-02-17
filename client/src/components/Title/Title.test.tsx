b573bf41dc94e54b28956ea332a3f6ea45bbc355
skp96 <patel.sunny293@gmail.com>
2022-02-17 15:18:49 -0500

    ipm demo

--- client/src/components/Title/Title.test.tsx
+++ client/src/components/Title/Title.test.tsx
@@ -3,6 +3,6 @@ import { Title } from "./Title"
 
 test("displays a welcome title", () => {
     const { getByText } = render(<Title />)
-    
-    expect(getByText(/welcome to wine-o inventory/i)).toBeInTheDocument
+
+    expect(getByText(/welcome to wine-o inventory, your favorite app/i)).toBeInTheDocument
 })
