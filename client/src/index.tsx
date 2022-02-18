f337de50132b1ebd6fb9f74584921804648c01a6
skp96 <patel.sunny293@gmail.com>
2022-02-18 17:30:39 -0500

    added sentry for error monitoring

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+if (process.env.NODE_ENV == 'production') {
+  Sentry.init({
+    dsn: "https://a9dad3cbcace4619bf0290155938f807@o1147590.ingest.sentry.io/6218055",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
