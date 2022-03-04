df5d8ea1e2795d4815b926d264f8e898c8ed4760
skp96 <patel.sunny293@gmail.com>
2022-03-04 13:09:23 -0500

    changed base url to point to aws

--- client/src/api/base_urls.tsx
+++ client/src/api/base_urls.tsx
@@ -1,1 +1,1 @@
-export const getWinesUrl = (process.env.NODE_ENV == 'production' ? "https://wine-o-inventory.herokuapp.com/api/wines" : "http://localhost:4000/api/wines")
+export const getWinesUrl = (process.env.NODE_ENV == 'production' ? "http://18.219.145.180:8080/api/wines" : "http://localhost:4000/api/wines")
